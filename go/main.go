94197bf9722f612f9c0370fe91f608ad595fc3a3
Kevin Cali <kevin@cali.sh>
2022-06-26 18:47:52 +0200

    added endpoint and http client to Go
    Signed-off-by: Kevin Cali <kevin@cali.sh>

--- go/main.go
+++ go/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/ping", pong)
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+var counter = 0
+
+func pong(w http.ResponseWriter, _ *http.Request) {
+	counter++
+	log.Printf("Go got called %d times!", counter)
+
+	_, err := fmt.Fprint(w, "Hi Kotlin!")
+	checkError(err)
+
+	_, err = http.Get("http://localhost:8888/ping")
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
 }
