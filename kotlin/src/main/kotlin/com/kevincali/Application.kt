6e25b69a624b7c98b7d0eedc161c05081795dd5f
Kevin Cali <kevin@cali.sh>
2022-06-26 18:47:25 +0200

    added endpoint and http client to Kotlin
    Signed-off-by: Kevin Cali <kevin@cali.sh>

--- kotlin/src/main/kotlin/com/kevincali/Application.kt
+++ kotlin/src/main/kotlin/com/kevincali/Application.kt
@@ -1,11 +1,36 @@
 package com.kevincali
 
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.kevincali.plugins.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main() {
-    embeddedServer(Netty, port = 8888, host = "0.0.0.0") {
+    embeddedServer(Netty, port = 8888) {
         configureRouting()
     }.start(wait = true)
+}
+
+fun Application.configureRouting() {
+    routing {
+        get("/ping") { pong(call) }
+    }
+}
+
+var counter = 0
+
+suspend fun pong(call: ApplicationCall) {
+    val log = call.application.environment.log
+    log.info("Kotlin got called ${++counter} times!")
+
+    call.respondText("Hey there, Go!")
+
+    runCatching {
+        HttpClient().get("http://localhost:9999/ping")
+    }.onFailure {
+        log.error("Error: ${it.message}")
+    }
 }
